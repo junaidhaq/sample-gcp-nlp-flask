9c9c442d85c2e31a622af97dbbe8882c3a216604
junaidhaq <74965465+junaidhaq@users.noreply.github.com>
2021-03-25 10:35:08 +0000

    Update set_gcp_env_vars.sh

--- language_api/set_gcp_env_vars.sh
+++ language_api/set_gcp_env_vars.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 export PROJECT_ID=$(gcloud config get-value core/project)
+echo PROJECT_ID = $PROJECT_ID
 
 # This should have been done manual already (one off)
 # gcloud iam service-accounts keys create ~/key.json --iam-account example@${PROJECT_ID}.iam.gserviceaccount.com
 
 export GOOGLE_APPLICATION_CREDENTIALS="/home/${USER}/key.json"
+echo GOOGLE_APPLICATION_CREDENTIALS = GOOGLE_APPLICATION_CREDENTIALS
