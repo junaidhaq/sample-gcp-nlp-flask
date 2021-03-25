65cb245cfad0ed8dcaf704312b1968a170fabfeb
Sukhster70 <sukhbir_bassi@yahoo.co.uk>
2021-03-25 14:07:26 +0000

    Included the remainder of the processes that need to be set up

--- language_api/set_gcp_env_vars.sh
+++ language_api/set_gcp_env_vars.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+echo "NOTE: make sure that you use source for this file"
+
 export PROJECT_ID=$(gcloud config get-value core/project)
 
 # Check if populated
@@ -8,10 +11,33 @@ then
 fi
 echo PROJECT_ID = $PROJECT_ID
 
-# This should have been done manual already (one off)
-# gcloud iam service-accounts keys create ~/key.json --iam-account example@${PROJECT_ID}.iam.gserviceaccount.com
+# Enable the APIs: (You can also do these through Navigation Menu -> APIs & Services)
 
+gcloud services enable language.googleapis.com
+gcloud services enable datastore.googleapis.com
+
+# Create a Service Account to access the Google Cloud APIs when testing locally:
+
+gcloud iam service-accounts create example \
+--display-name "My Service Account"
+
+# Give your newly created Service Account appropriate permissions:
+
+gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+--member serviceAccount:example@${PROJECT_ID}.iam.gserviceaccount.com \
+--role roles/owner
+
+# After creating your Service Account, create a Service Account key:
+
+gcloud iam service-accounts keys create ~/key.json --iam-account \
+example@${PROJECT_ID}.iam.gserviceaccount.com
+
+#Set the GOOGLE_APPLICATION_CREDENTIALS environment variable to point to where you just put your Service Account key:
 export GOOGLE_APPLICATION_CREDENTIALS="/home/${USER}/key.json"
-echo GOOGLE_APPLICATION_CREDENTIALS = $GOOGLE_APPLICATION_CREDENTIALS
+
+echo "Start Python Virtual Environment"
+virtualenv -p python3 env
+source env/bin/activate
+pip install -r requirements.txt
 
 echo "Finished"
